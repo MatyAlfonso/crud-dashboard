d1317e7b5ba3db75de3263f36071e18c30fa249c
MatyAlfonso <matt1998boca@gmail.com>
2023-01-04 18:19:29 +0100

    Create frontend folder with React-Redux setup

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className='text-6xl text-red-600'>My app</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
