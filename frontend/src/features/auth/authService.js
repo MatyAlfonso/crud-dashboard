b38950397a3115604428ec21bea203f7f294cb61
MatyAlfonso <matt1998boca@gmail.com>
2023-01-11 19:10:26 +0100

    Delete proxy on package.json and add complete URL on the API_URL variable

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/users/';
+const API_URL = 'https://crud-dashboard-production.up.railway.app/api/users/';
 
 // Login user 
 const login = async (userData) => {
--- frontend/src/features/giveaways/giveawaysService.js
+++ frontend/src/features/giveaways/giveawaysService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/giveaways/';
+const API_URL = 'https://crud-dashboard-production.up.railway.app/api/giveaways/';
 
 // Get giveaways 
 const getGiveaways = async (token) => {
